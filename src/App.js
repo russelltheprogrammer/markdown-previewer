18f9a89fdd2a868d1efabb42929634291aa2ec19
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-06-16 11:35:40 -0400

    Start Work on Markdown Previewer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { Editor } from "./Editor";
+
 
 function App() {
+
+const [ state, setState] = useState({
+  text: ""
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        In Process
+      <header id="header">
+        Markdown Previewer
       </header>
+      <div className="contentOfMyApp">
+
+        <Editor values={} onChange={} />
+        <form id="markDownForm">
+
+      <label>
+        Editor:
+        <textarea 
+        id="editor" 
+        rows="10" 
+        cols="45"
+        name="text"
+        placeholder="Insert Text Here"
+        type="text"
+        value={state.text}
+        onChange={handleChange} />
+      </label>
+
+      
+      </form>
+      <div id="preview">
+        {state.text}
+      Preview
+      </div>
+      </div>
     </div>
   );
 }
